d20cc2e031301f20ab95cc041b4462dc91f6019b
Ragh7av <harekrishna79english@gmail.com>
2026-09-20 09:18:21 +0530

    Create room.js

---
+++ public/room.js
@@ -0,0 +1,11 @@
+const roomTitle = document.getElementById("room-title");
+const roomCodeDisplay = document.getElementById("room-code-display");
+
+const copyCodeButton = document.getElementById("copy-code");
+const leaveRoombutton = document.getElementById("leave-room");
+
+const messageInput = document.getElementById("message-input");
+const sendMessageButton = document.getElementById("send-message");
+const chatMessageButton = document.getElementById("chat-messages");
+
+const savedRoom = localStorage.getItem("currentRoom");
